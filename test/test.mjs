1ef7e296aee8fcc5c6ca9196c607cd8619b92011
Avaer Kazmer <a@modules.io>
2021-11-14 16:45:09 -0500

    Add zmap tests

--- test/test.mjs
+++ test/test.mjs
@@ -1,6 +1,31 @@
 import assert from 'assert';
 import Z from '../z.mjs';
 
+describe('ZMap', function() {
+  describe('basic', function() {
+    it('should support basic operations', function() {
+      const doc = new Z.Doc();
+      const map = doc.getMap('map');
+      
+      map.set('key', 'value');
+      assert.equal(map.get('key'), 'value');
+      assert.equal(map.get('key2'), undefined);
+      
+      const keys = Array.from(map.keys());
+      assert.deepEqual(keys, ['key']);
+      
+      const values = Array.from(map.values());
+      assert.deepEqual(values, ['value']);
+      
+      const entries = Array.from(map.entries());
+      assert.deepEqual(values, ['key', 'value']);
+      
+      map.set('key2', 'value2');
+      assert.equal(map.get('key2'), 'value2');
+    });
+  });
+});
+
 describe('ZArray', function() {
   describe('basic', function() {
     it('should support basic operations', function() {
